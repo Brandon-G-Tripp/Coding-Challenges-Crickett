1461d507cd51bf98f3408338149c8765d4c825e0
Brandon Tripp <brandongtripp@gmail.com>
2024-04-23 17:36:55 -0600

    Elixir: tests for development of the -l flag for wc project

--- wc-clone/wc_elixir/test/wc_test.exs
+++ wc-clone/wc_elixir/test/wc_test.exs
@@ -17,5 +17,20 @@ defmodule WcTest do
     end
   end
 
+  test "count_lines/1 counts the number of lines in a file" do 
+    content = "Line 1\nLine 2\nLine 3\n"
+    file_name = "test.txt"
+    File.write!(file_name, content)
+    assert Wc.count_lines(file_name) == 3
+    File.rm!(file_name)
+  end
+
+  test "count_lines/1 returns an error for a non-existent file" do 
+    file_name = "nonexistent.txt"
+    assert_raise File.Error, fn -> 
+      Wc.count_lines(file_name)
+    end
+  end
+
 end
 
