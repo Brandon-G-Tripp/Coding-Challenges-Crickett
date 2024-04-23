8c7460216a0ef5ddf3e50ad486951d66c7f77ecc
Brandon Tripp <brandongtripp@gmail.com>
2024-04-23 13:43:38 -0600

    Clang: correct parsing cli args loop with break and refactor main function to main.c

--- wc-clone/wc-c/wc.c
+++ wc-clone/wc-c/wc.c
@@ -15,42 +15,21 @@ int count_bytes(const char* file_path) {
     return (int)count;
 } 
 
-int main(int argc, char* argv[]) {
-    int opt; 
-    int count_bytes_flag = 0;
-
-    while ((opt = getopt(argc, argv, "c")) != -1) {
-        switch (opt) {
-            case 'c':
-                count_bytes_flag = 1;
-                break;
-            default:
-                fprintf(stderr, "Usage: %s [-c] <file>\n", argv[0]);
-                exit(EXIT_FAILURE);
-        } 
+int count_lines(const char* file_path) {
+    FILE* file = fopen(file_path, "r");
+    if (file == NULL) {
+        return -1;
     } 
 
-    if (optind >= argc) {
-        fprintf(stderr,"Missing file argument\n");
-        fprintf(stderr, "Usage: %s [-c] <file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    } 
-
-    const char* file_path = argv[optind];
-
-    if (count_bytes_flag) {
-        int count = count_bytes(file_path);
-        if (count == -1) {
-            fprintf(stderr, "Error: Could not open file '%s'\n", file_path);
-            exit(EXIT_FAILURE);
+    int count = 0;
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            count++;
         } 
-        printf("%d %s\n", count, file_path);
-    } else {
-        fprintf(stderr, "Error: Missing -c flag\n");
-        fprintf(stderr, "Usage: %s [-c] <file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    } 
+    fclose(file);
 
-    return 0;
+    return count;
 }
 
