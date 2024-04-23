1f2e03eda2c392232a6924da75c0e59dbe88ae11
Brandon Tripp <brandongtripp@gmail.com>
2024-04-23 01:17:40 -0600

    OCaml: cli parsing for wc project to read bytes of a file

--- wc-clone/wc-ocaml/wc.ml
+++ wc-clone/wc-ocaml/wc.ml
@@ -3,3 +3,23 @@ let count_bytes file_name =
     let len = in_channel_length ic in
     close_in ic;
     len
+
+
+let main () = 
+    let bytes_flag = ref false in
+    let spec = [("-c", Arg.Set bytes_flag, "Count bytes")] in
+    let usage = "Usage: wc [-c] <file>" in
+    Arg.parse spec (fun _ -> ()) usage;
+    let file_name = Sys.argv.(Array.length Sys.argv - 1) in
+    if !bytes_flag then
+        try 
+            let count = count_bytes file_name in
+            Printf.printf "%d %s\n" count file_name
+        with Sys_error _ -> 
+            Printf.eprintf "Error: Could not open file '%s'\n" file_name;
+            exit 1
+    else
+        (Printf.eprintf "Error: Missing -c flag \n";
+        exit 1)
+
+let () = main ()
