85705c2e19d2de08055bfa0fa9d75b1c04918098
Brandon Tripp <brandongtripp@gmail.com>
2024-04-23 00:59:59 -0600

    Golang: cli parsing to count bytes of file in wc project

--- wc-clone/wc-go/wc.go
+++ wc-clone/wc-go/wc.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-    "io/ioutil"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func countBytes(filePath string) (int, error) {
@@ -10,4 +13,28 @@ func countBytes(filePath string) (int, error) {
         return 0, err
     }
     return len(data), nil
+} 
+
+func main() {
+    countBytesFlag := flag.Bool("c", false, "Count bytes")
+    flag.Parse()
+
+    if flag.NArg() == 0 {
+        fmt.Println("Please provide a file")
+        os.Exit(1)
+    } 
+
+    filePath := flag.Arg(0)
+
+    if *countBytesFlag {
+        count, err := countBytes(filePath)
+        if err != nil {
+            fmt.Printf("Error: Could not open file '%s'\n", filePath)
+            os.Exit(1)
+        }
+        fmt.Printf("%d %s\n", count, filePath)
+    } else {
+        fmt.Println("Error: Missing -c flag")
+        os.Exit(1)
+    }
 } 
