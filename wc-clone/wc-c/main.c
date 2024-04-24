2864eb2738a4ef17bc0447ef2ef4a13da0e706f0
Brandon Tripp <brandongtripp@gmail.com>
2024-04-23 21:10:55 -0600

    Clang: implement handling of the -w word count flag in main func

--- wc-clone/wc-c/main.c
+++ wc-clone/wc-c/main.c
@@ -4,13 +4,15 @@
 
 extern int count_bytes(const char* file_path);
 extern int count_lines(const char* file_path);
+extern int count_words(const char* file_path);
 
 int main(int argc, char* argv[]) {
     int opt; 
     int count_bytes_flag = 0;
     int count_lines_flag = 0;
+    int count_words_flag = 0;
 
-    while ((opt = getopt(argc, argv, "cl")) != -1) {
+    while ((opt = getopt(argc, argv, "clw")) != -1) {
         switch (opt) {
             case 'c':
                 count_bytes_flag = 1;
@@ -18,15 +20,18 @@ int main(int argc, char* argv[]) {
             case 'l':
                 count_lines_flag = 1;
                 break;
+            case 'w':
+                count_words_flag = 1;
+                break;
             default:
-                fprintf(stderr, "Usage: %s [-c] [-l] <file>\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-c] [-l] [-w] <file>\n", argv[0]);
                 exit(EXIT_FAILURE);
         } 
     } 
 
     if (optind >= argc) {
         fprintf(stderr,"Missing file argument\n");
-        fprintf(stderr, "Usage: %s [-c] <file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s [-c] [-l] [-w]<file>\n", argv[0]);
         exit(EXIT_FAILURE);
     } 
 
@@ -39,11 +44,29 @@ int main(int argc, char* argv[]) {
             exit(EXIT_FAILURE);
         } 
         printf("%d %s\n", count, file_path);
-    } else if (!count_bytes_flag) {
-        fprintf(stderr, "Error: Missing -c or -l flag\n");
-        fprintf(stderr, "Usage: %s [-c] [-l] <file>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    } 
+    if (count_bytes_flag) {
+        int count = count_lines(file_path);
+        if (count == -1) {
+            fprintf(stderr, "Error: Could not open file '%s'\n", file_path);
+            exit(EXIT_FAILURE);
+        } 
+        printf("%d %s\n", count, file_path);
     }
+    if (count_words_flag) {
+        int count = count_words(file_path);
+        if (count == -1) {
+            fprintf(stderr, "Error: Could not open file '%s'\n", file_path);
+            exit(EXIT_FAILURE);
+        } 
+        printf("%d %s\n", count, file_path);
+    }
+
+    if (!count_bytes_flag && !count_lines_flag && !count_words_flag) {
+        fprintf(stderr, "Error: Missing -c, -l, or -w flag\n");
+        fprintf(stderr, "Usage: %s [-c] [-l] [-w] <file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    } 
 
     return 0;
 }
