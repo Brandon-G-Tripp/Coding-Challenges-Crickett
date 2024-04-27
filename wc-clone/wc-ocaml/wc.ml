f7479c7ede86848e83dbdf6c64a3b6420ebd1420
Brandon Tripp <brandongtripp@gmail.com>
2024-04-26 23:32:47 -0600

    OCaml: Test and functionality for the -m charactewr count flag for the wc project

--- wc-clone/wc-ocaml/wc.ml
+++ wc-clone/wc-ocaml/wc.ml
@@ -1,3 +1,18 @@
+let count_chars file_name = 
+    let ic = open_in file_name in
+    let count = ref 0 in 
+    let decoder = Uutf.decoder ~encoding: `UTF_8 (`Channel ic) in
+    let rec loop () = 
+        match Uutf.decode decoder with
+        | `Await -> close_in ic; !count
+        | `End -> close_in ic; !count
+        | `Malformed _ -> close_in ic; raise (Failure "Malformed UTF-8 data")
+        | `Uchar _ -> 
+                incr count;
+                loop () 
+    in
+    loop ()
+
 let count_bytes file_name = 
     let ic = open_in_bin file_name in
     let len = in_channel_length ic in
@@ -35,13 +50,15 @@ let run_main () =
     let bytes_flag = ref false in
     let lines_flag = ref false in
     let words_flag = ref false in
+    let chars_flag = ref false in
     let file_name = ref "" in
     let spec = [
         ("-c", Arg.Set bytes_flag, "Count bytes");
         ("-l", Arg.Set lines_flag, "Count lines");
         ("-w", Arg.Set words_flag, "Count words");
+        ("-m", Arg.Set chars_flag, "Count characters");
     ] in
-    let usage = "Usage: wc [-c] [-l] <file>" in
+    let usage = "Usage: wc [-c] [-l] [-w] [-m] <file>" in
     let anon_fun s = file_name := s in
     Arg.parse spec anon_fun usage;
     if !file_name = "" then
@@ -49,7 +66,10 @@ let run_main () =
         exit 1)
     else
         try
-            if !bytes_flag then
+            if !chars_flag then
+                let count = count_chars !file_name in
+                Printf.printf "%d %s\n" count !file_name
+            else if !bytes_flag then
                 let count = count_bytes !file_name in
                 Printf.printf "%d %s\n" count !file_name
             else if !lines_flag then
--- wc-clone/wc-ocaml/wc_test.ml
+++ wc-clone/wc-ocaml/wc_test.ml
@@ -52,6 +52,23 @@ let test_count_words_file_not_found _ =
         assert_failure "Expected an exception, but none was raised"
     with Sys_error _ -> ()
 
+let test_count_chars _ = 
+    let content = "Sample content with 🚀 emoji" in
+    let file_name = "test.txt" in 
+    let oc = open_out file_name in 
+    output_string oc content;
+    close_out oc;
+    let count = count_chars file_name in 
+    assert_equal 27 count;
+    Sys.remove file_name
+
+let test_count_chars_file_not_found _ = 
+    let file_name = "nonexistent.txt" in 
+    try 
+        let _ = count_chars file_name in 
+        assert_failure "Expected an exception, but none was raised"
+    with Sys_error _ -> ()
+
 let suite = 
     "CountBytes tests" >::: [
         "test_count_bytes" >:: test_count_bytes;
@@ -60,6 +77,8 @@ let suite =
         "test_count_lines_file_not_found" >:: test_count_lines_file_not_found;
         "test_count_words" >:: test_count_words;
         "test_count_words_file_not_found" >:: test_count_words_file_not_found;
+        "test_count_chars" >:: test_count_chars;
+        "test_count_chars_file_not_found" >:: test_count_chars_file_not_found;
     ]
 
 let () = 
