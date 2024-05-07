8980e6709816974219646589d97a7f16a19f5583
Brandon Tripp <brandongtripp@gmail.com>
2024-05-07 13:19:02 -0600

    Python: stage 2 of json parser, tests and implementation to handle key value pairs with string values

--- json-parser/python_json_parser/src/parser.py
+++ json-parser/python_json_parser/src/parser.py
@@ -14,11 +14,52 @@ def parse_json(input_string):
             return True
         return False
 
+    def parse_string():
+        nonlocal index
+        if not consume_char('"'):
+            return False
+        while index < len(input_string) and input_string[index] != '"':
+            if input_string[index] =='\\':
+                index += 1
+            index += 1
+        if not consume_char('"'):
+            return False
+        return True
+
+    def parse_key_value_pair():
+        if not parse_string():
+            return False
+        if not consume_char(':'):
+            return False
+        if not parse_value():
+            return False
+        return True
+
+    def parse_value():
+        consume_whitespace()
+        if parse_string():
+            return True
+        return False
+
     def parse_object():
         if not consume_char('{'):
             return False
+        consume_whitespace()
+        if consume_char('}'):
+            consume_whitespace()
+            return index == len(input_string)
+        if not parse_key_value_pair():
+            return False
+        consume_whitespace()
+        while consume_char(','):
+            consume_whitespace()
+            if not parse_key_value_pair():
+                return False
+            consume_whitespace()
+        consume_whitespace()
         if not consume_char('}'):
             return False
-        return True
+        consume_whitespace()
+        return index == len(input_string)
 
     return parse_object()
--- json-parser/python_json_parser/tests/test_parser.py
+++ json-parser/python_json_parser/tests/test_parser.py
@@ -1,12 +1,38 @@
-import os 
+import os
 from src.parser import parse_json
+
 
 def test_parse_valid_json():
     with open(os.path.join(os.path.dirname(__file__), 'step1', 'valid.json'), 'r') as file:
         valid_json = file.read()
         assert parse_json(valid_json) == True
 
+
 def test_parse_invalid_json():
     with open(os.path.join(os.path.dirname(__file__), 'step1', 'invalid.json'), 'r') as file:
+        invalid_json = file.read()
+        assert parse_json(invalid_json) == False
+
+
+def test_parse_valid_simple_object():
+    with open(os.path.join(os.path.dirname(__file__), 'step2', 'valid.json'), 'r') as file:
+        valid_json = file.read()
+        assert parse_json(valid_json) == True
+
+
+def test_parse_valid_object_with_multiple_pairs():
+    with open(os.path.join(os.path.dirname(__file__), 'step2', 'valid2.json'), 'r') as file:
+        valid_json = file.read()
+        assert parse_json(valid_json) == True
+
+
+def test_parse_invalid_object_missing_key():
+    with open(os.path.join(os.path.dirname(__file__), 'step2', 'invalid.json'), 'r') as file:
+        invalid_json = file.read()
+        assert parse_json(invalid_json) == False
+
+
+def test_parse_invalid_object_with_invalid_key():
+    with open(os.path.join(os.path.dirname(__file__), 'step2', 'invalid2.json'), 'r') as file:
         invalid_json = file.read()
         assert parse_json(invalid_json) == False
