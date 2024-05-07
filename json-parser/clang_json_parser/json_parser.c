c711cb7c33bde0c9e26bf65677403bcf775f7a9e
Brandon Tripp <brandongtripp@gmail.com>
2024-05-06 18:10:53 -0600

    Clang: stage two of json parser project, handling basic key value pairs of json.

--- json-parser/clang_json_parser/json_parser.c
+++ json-parser/clang_json_parser/json_parser.c
@@ -1,28 +1,77 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "json_parser.h"
 
 bool is_valid_json(const char *json) {
-    size_t len = strlen(json);
-    size_t i = 0;
+    const char *ptr = json;
 
-    while (i < len && (json[i] == ' ' || json[i] == '\t' || json[i] == '\n' || json[i] == '\r')) {
-        i++;
+    while (isspace(*ptr)) {
+        ptr++;
+    } 
+
+    if (*ptr != '{') {
+        return false;
     }
+    ptr++;
 
-    if (i >= len || json[i] != '{') {
-        return false;
+    while(isspace(*ptr)) {
+        ptr++;
     } 
 
-    while (len > 0 && (json[len - 1] == ' ' || json[len - 1] == '\t' || json[len - 1] == '\n' || json[len - 1] == '\r')) {
-        len--;
+    while (*ptr != '\0') {
+        if (*ptr != '"') {
+            return false;
+        }
+        ptr++;
+        while (*ptr != '"') {
+            if (*ptr == '\0') {
+                return false;
+            }
+            ptr++;
+        }
+        ptr++;
+
+        while (isspace(*ptr)) {
+            ptr++;
+        }
+
+        if (*ptr != ':') {
+            return false;
+        }
+        ptr++;
+
+        while (isspace(*ptr)) {
+            ptr++;
+        }
+
+        if (*ptr != '"') {
+            return false;
+        }
+        ptr++;
+        while (*ptr != '"') {
+            if (*ptr == '\0') {
+                return false;
+            }
+            ptr++;
+        }
+        ptr++;
+
+        while (isspace(*ptr)) {
+            ptr++;
+        }
+
+        if (*ptr == ',') {
+            ptr++;
+        } else if (*ptr == '}') {
+            return true;
+        } else {
+            return false;
+        }
     } 
 
-    if (len == 0 || json[len - 1] != '}') {
-        return false;
-    } 
-
-    return true;
+    return false;
 } 
 
 void print_validation_result(bool is_valid) {
--- json-parser/clang_json_parser/json_parser_test.c
+++ json-parser/clang_json_parser/json_parser_test.c
@@ -15,7 +15,6 @@ void test_valid_empty_object() {
 void test_valid_empty_object_with_whitespace() {
     const char *json = " \t\n{}\r\n ";
     bool is_valid = is_valid_json(json);
-
     if (is_valid) {
         printf("Test passed: Valid empty object with whitespace\n");
     } else {
@@ -34,7 +33,7 @@ void test_invalid_json_missing_closing_brace() {
 }
 
 void test_invalid_json_missing_opening_brace() {
-    const char *json = "{";
+    const char *json = "}";
     bool is_valid = is_valid_json(json);
     if (!is_valid) {
         printf("Test passed: Invalid JSON missing opening brace\n");
@@ -43,10 +42,65 @@ void test_invalid_json_missing_opening_brace() {
     }
 }
 
+void test_valid_json_object_with_single_pair() {
+    const char *json = "{\"key\":\"value\"}";
+    bool is_valid = is_valid_json(json);
+    if (is_valid) {
+        printf("Test passed: Valid JSON object with single pair\n");
+    } else {
+        printf("Test failed: Valid JSON object with single pair\n");
+    }
+}
+
+void test_valid_json_object_with_multiple_pairs() {
+    const char *json = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
+    bool is_valid = is_valid_json(json);
+    if (is_valid) {
+        printf("Test passed: Valid JSON object with multiple pairs\n");
+    } else {
+        printf("Test failed: Valid JSON object with multiple pairs\n");
+    }
+}
+
+void test_invalid_json_object_missing_key() {
+    const char *json = "{:\"value\"}";
+    bool is_valid = is_valid_json(json);
+    if (!is_valid) {
+        printf("Test passed: Invalid JSON object missing key\n");
+    } else {
+        printf("Test failed: Invalid JSON object missing key\n");
+    }
+}
+
+void test_invalid_json_object_missing_value() {
+    const char *json = "{\"key\":}";
+    bool is_valid = is_valid_json(json);
+    if (!is_valid) {
+        printf("Test passed: Invalid JSON object missing value\n");
+    } else {
+        printf("Test failed: Invalid JSON object missing value\n");
+    }
+}
+
+void test_invalid_json_object_missing_colon() {
+    const char *json = "{\"key\"\"value\"}";
+    bool is_valid = is_valid_json(json);
+    if (!is_valid) {
+        printf("Test passed: Invalid JSON object missing colon\n");
+    } else {
+        printf("Test failed: Invalid JSON object missing colon\n");
+    }
+}
+
 int main() {
     test_valid_empty_object();
     test_valid_empty_object_with_whitespace();
     test_invalid_json_missing_closing_brace();
     test_invalid_json_missing_opening_brace();
+    test_valid_json_object_with_single_pair();
+    test_valid_json_object_with_multiple_pairs();
+    test_invalid_json_object_missing_key();
+    test_invalid_json_object_missing_value();
+    test_invalid_json_object_missing_colon();
     return 0;
 } 
