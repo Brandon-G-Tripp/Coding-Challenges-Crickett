7a40c9344bf097c7ee5aaffbdc08161fc4f12e52
Brandon Tripp <brandongtripp@gmail.com>
2024-05-08 22:13:38 -0600

    OCaml: stage 3 of json parser project, implementation and handling of numeric, string, boolean and null values

--- json-parser/ocaml_json_parser/json_parser/lib/parser.ml
+++ json-parser/ocaml_json_parser/json_parser/lib/parser.ml
@@ -28,11 +28,62 @@ let parse_json input_string =
         )
     in
 
+
+    let parse_number () = 
+        let start_index = !index in
+        let is_valid = ref true in
+        if consume_char '-' then ();
+        if not (consume_char '0') then (
+            if !index < len && input_string.[!index] >= '1' && input_string.[!index] <= '9' then
+                while !index < len && input_string.[!index] >= '0' && input_string.[!index] <= '9' do 
+                    incr index
+                done
+            else
+                is_valid := false
+        );
+        if !index < len && input_string.[!index] = '.' then (
+            incr index;
+            if !index < len && input_string.[!index] >= '0' && input_string.[!index] <= '9' then
+                while !index < len && input_string.[!index] >= '0' && input_string.[!index] <= '9' do 
+                    incr index
+                done
+            else
+                is_valid := false
+        );
+        if !index < len && (input_string.[!index] = 'e' || input_string.[!index] = 'E') then (
+            incr index;
+            if !index < len && (input_string.[!index] = '+' || input_string.[!index] = '-') then
+                incr index;
+            if !index <len && input_string.[!index] >= '0' && input_string.[!index] <= '9' then
+                while !index < len && input_string.[!index] >= '0' && input_string.[!index] <= '9' do 
+                    incr index
+                done
+            else
+                is_valid := false
+        );
+        !is_valid && !index > start_index
+    in
+
+    let parse_value () = 
+        consume_whitespace ();
+        if !index < len then
+            match input_string.[!index] with 
+            | '"' -> parse_string ()
+            | 't' -> consume_char 't' && consume_char 'r' && consume_char 'u' && consume_char 'e'
+            | 'f' -> consume_char 'f' && consume_char 'a' && consume_char 'l' && consume_char 's' && consume_char 'e'
+            | 'n' -> consume_char 'n' && consume_char 'u' && consume_char 'l' && consume_char 'l'
+            | '-' | '0'..'9' -> parse_number ()
+            | _ -> false
+        else
+            false
+    in
+
     let parse_key_value_pair () = 
         if not (parse_string ()) then false
         else if not (consume_char ':') then false
-        else parse_string ()
+        else parse_value ()
     in
+
 
     let rec parse_pairs () = 
         consume_whitespace ();
@@ -57,4 +108,6 @@ let parse_json input_string =
         ) else 
             false
     )
+
+
 
--- json-parser/ocaml_json_parser/json_parser/test/test_parser.ml
+++ json-parser/ocaml_json_parser/json_parser/test/test_parser.ml
@@ -25,6 +25,26 @@ let test_parse_invalid_object _ =
     let json = {|{"key": "value",}|} in
     assert_equal false (parse_json json)
 
+let test_parse_string_value _ = 
+    let json = {|{"key": "value"}|} in
+    assert_equal true (parse_json json)
+    
+let test_parse_numeric_value _ = 
+    let json = {|{"key": 42}|} in
+    assert_equal true (parse_json json)
+
+let test_parse_boolean_value  _ = 
+    let json = {|{"key": true}|} in
+    assert_equal true (parse_json json)
+
+let test_parse_null_value _ = 
+    let json = {|{"key": null}|} in
+    assert_equal true (parse_json json)
+
+let test_parse_all_value_types _ =
+    let json = {|{"key1": "string", "key2": 42, "key3": true, "key4": false, "key5": null}|} in
+    assert_equal true (parse_json json)
+
 let suite = 
     "Parser tests" >::: [
         "test_parse_valid_json" >:: test_parse_valid_json;
@@ -33,6 +53,11 @@ let suite =
         "test_parse_single_pair_object" >:: test_parse_single_pair_object;
         "test_parse_multi_pair_object" >:: test_parse_multi_pair_object;
         "test_parse_invalid_object" >:: test_parse_invalid_object;
+        "test_parse_string_value" >:: test_parse_string_value;
+        "test_parse_numeric_value" >:: test_parse_numeric_value;
+        "test_parse_boolean_value" >:: test_parse_boolean_value;
+        "test_parse_null_value" >:: test_parse_null_value;
+        "test_parse_all_value_types" >:: test_parse_all_value_types;
     ]
 
 let _ = run_test_tt_main suite
